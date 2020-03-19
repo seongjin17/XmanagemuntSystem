2ea356598272800197bc71d61294c857bf8ee853
tjdwl <tjdwl@DESKTOP-JCAVEVC>
2020-03-19 10:39:28 +0900

    Create a menu

---
+++ src/Helloworld.java
@@ -0,0 +1,16 @@
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+		System.out.println("Selet one number between 1-5:");
+		System.out.println("1, Add student");
+	 	System.out.println("2, Delete student");
+	 	System.out.println("3, Edit student");
+	 	System.out.println("4, View student");
+	 	System.out.println(" Exit");
+	 	
+				
+	}
+	
+
+}

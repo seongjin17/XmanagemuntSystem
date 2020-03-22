5bf6f35d488c9dabf0353ac8ae3b2e08ea15bcf9
tjdwl <tjdwl@DESKTOP-JCAVEVC>
2020-03-22 23:45:37 +0900

    menu

---
+++ src/temperature.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class temperature {
+	public static void main(String[] args) {
+	  System.out.println("¼·¾¾¿Âµµ=");		
+	  Scanner input = new Scanner (System.in);
+	  double f = input.nextDouble (); 
+      double c;
+      c = 5.0/9*(f-32);
+      System.out.println("È­¾¾¿Âµµ=" +c);	
+   } 
+}

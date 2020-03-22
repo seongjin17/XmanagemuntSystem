46b00b531a1d0facb161df1bd326577ca9a0f0ba
tjdwl <tjdwl@DESKTOP-JCAVEVC>
2020-03-22 23:22:35 +0900

    menu

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -3,11 +3,11 @@ public class MenuManager {
 
 	public static void main(String[] args) {
 		System.out.println("Selet one number between 1-5:");
-		System.out.println("1, Add student");
-	 	System.out.println("2, Delete student");
-	 	System.out.println("3, Edit student");
-	 	System.out.println("4, View student");
-	 	System.out.println(" Exit");
+		System.out.println("1, Add character");
+	 	System.out.println("2, Delete character");
+	 	System.out.println("3, Edit character");
+	 	System.out.println("4, View character");
+	 	System.out.println("Exit");
 	 	
 				
 	}

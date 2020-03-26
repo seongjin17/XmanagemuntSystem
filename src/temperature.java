4606b8e2aad6dbfdc257c9de8cb775b745707efe
tjdwl <tjdwl@DESKTOP-JCAVEVC>
2020-03-26 16:19:08 +0900

    menu

--- src/temperature.java
+++ src/temperature.java
@@ -4,9 +4,9 @@ public class temperature {
 	public static void main(String[] args) {
 	  System.out.println("¼·¾¾¿Âµµ=");		
 	  Scanner input = new Scanner (System.in);
-	  double f = input.nextDouble (); 
+	  int f = input.nextInt (); 
       double c;
       c = 5.0/9*(f-32);
-      System.out.println("È­¾¾¿Âµµ=" +c);	
+      System.out.println("È­¾¾ ¿Âµµ=" +c);	
    } 
 }

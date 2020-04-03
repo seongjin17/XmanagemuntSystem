34263a714dffd28644294c6d74bef22f3313e984
tjdwl <tjdwl@DESKTOP-JCAVEVC>
2020-04-03 13:11:21 +0900

    create Edit menu

--- src/Charactermanager.java
+++ src/Charactermanager.java
@@ -21,12 +21,11 @@ public class Charactermanager {
 	}
 	public void deletechatacter() {
 		System.out.print("character nicname:");
-	    String characternicname = input.next();
-	    if(character == null) {
+	    String nicname= input.next();
+	    if(nicname.equals(null)) {
 	    	System.out.println("the character has not been registered");
-	    	return;
 	    }
-	    if(character.nicname == characternicname) {
+	    if(character.nicname.equals(nicname)) {
 	    	character = null;
 	    	System.out.println("the character is deleted");
 	    }
@@ -34,16 +33,51 @@ public class Charactermanager {
 	}
 	public void editcharacter() {
 		System.out.print("character nicname:");
-	    String characternicname = input.next();
-	    if(character.nicname == characternicname) {
-	    	System.out.println("the character to be edited is" + characternicname);
+	    String nicname = input.next();
+	    if(character.nicname.equals(nicname)) {
+	    	int num = -1;
+			while(num !=5) {
+				System.out.println("** Character Info Edit Menu **");
+				System.out.println("1, Edit nicname");
+				System.out.println("2, Edit level ");
+				System.out.println("3, Edit power");
+				System.out.println("4, Edit job");
+				System.out.println("5, Exit");
+				System.out.println("Select one number between 1-5:");
+				num = input.nextInt();
+				if(num == 1) {
+					System.out.print("character nicname:");
+					character.nicname = input.next();
+					
+				}	
+				else if(num == 2) {
+					System.out.print("character level:");
+				    character.level = input.nextInt();
+				   
+				}
+				else if(num == 3) {
+					character.level = input.nextInt();
+				    System.out.print("character power:");
+				    
+				}
+				else if(num == 4 ) {
+					character.power = input.nextInt();
+				    System.out.print("character job:");
+				   
+				}
+				else {
+					continue;
+				}
+			}	
+	    	
 	    }
 	}
 	public void viewcharacter() {
 		System.out.print("character nicname:");
-	    String characternicname = input.next();
-	    if(character.nicname == characternicname) {
+	    String nicname = input.next();
+	    if(character.nicname.equals(nicname)) {
 	    	character.printinfo();
+	    	
 	    }
 	}    
 }

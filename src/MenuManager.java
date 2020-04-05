6a3f2989b5f9ac7a1ac7c0e39fa81da07dcfbd29
tjdwl <tjdwl@DESKTOP-JCAVEVC>
2020-04-05 10:56:54 +0900

    change

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -7,12 +7,12 @@ public class MenuManager {
 		int num = -1;
 		
 		while(num !=5) {
-			System.out.println("Selet one number between 1-5:");
+			System.out.println("***Character Management System Menu***");
 			System.out.println("1, Add character");
 			System.out.println("2, Delete character");
 			System.out.println("3, Edit character");
 			System.out.println("4, View character");
-			System.out.println("Exit");
+			System.out.println("5, Exit");
 			System.out.println("Select one number between 1-5:");
 			num = input.nextInt();
 			if(num == 1) {

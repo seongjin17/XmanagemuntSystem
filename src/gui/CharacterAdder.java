5e2c65180c1e6e21629af4f06fbdede6938e070c
tjdwl <tjdwl@DESKTOP-JCAVEVC>
2020-05-29 22:19:07 +0900

    +gui

---
+++ src/gui/CharacterAdder.java
@@ -0,0 +1,48 @@
+package gui;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SpringLayout;
+
+public class CharacterAdder extends JFrame{
+	public CharacterAdder() {
+		JPanel panel = new JPanel();
+		panel.setLayout(new SpringLayout());
+		
+		JLabel labelNicname = new JLabel("Nicname:",JLabel.TRAILING);
+		JTextField fieldNicname = new JTextField(10);
+		labelNicname.setLabelFor(fieldNicname);
+		panel.add(labelNicname);
+		panel.add(fieldNicname);
+		
+		JLabel labelLevel = new JLabel("Level:",JLabel.TRAILING);
+		JTextField fieldLevel = new JTextField(10);
+		labelLevel.setLabelFor(fieldLevel);
+		panel.add(labelLevel);
+		panel.add(fieldLevel);
+		
+		JLabel labelPower = new JLabel("Power:",JLabel.TRAILING);
+		JTextField fieldPower = new JTextField(10);
+		labelPower.setLabelFor(fieldPower);
+		panel.add(labelPower);
+		panel.add(fieldPower);
+		
+		JLabel labelJob = new JLabel("Job:",JLabel.TRAILING);
+		JTextField fieldJob = new JTextField(10);
+		labelJob.setLabelFor(fieldJob);
+		panel.add(labelJob);
+		panel.add(fieldJob);
+		
+		panel.add(new JButton("Add chracter"));
+		panel.add(new JButton("back"));
+		
+		SpringUtilities.makeCompactGrid(panel, 5, 2, 6, 6, 6, 6);
+		this.setSize(300, 300);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setContentPane(panel);
+		this.setVisible(true);
+	}
+}
---
+++ src/gui/GuiTest.java
@@ -0,0 +1,11 @@
+package gui;
+
+public class GuiTest {
+
+	public static void main(String[] args) {
+		//MenuSelection menuselection = new MenuSelection();
+		//CharacterAdder characteradder = new CharacterAdder();
+		Characterviewer characterviewer = new Characterviewer();
+	}
+
+}
